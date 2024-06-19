78d497da7dcd968a6451b0facfb3ebea1b53512b
Christopher McDonald <c.robin.mcdonald@gmail.com>
2024-06-19 11:22:39 +1000

    Add qrcode

--- src/routes/pages/about.tsx
+++ src/routes/pages/about.tsx
@@ -1,8 +1,12 @@
+import React from 'react'
+import  qrcode from '../../assets/meetup-rtl-github.svg' 
 
 function About() {
   return (
     <>
       <h1>About Page</h1>
+      <img src={qrcode} alt="github-code" width={400} height={400} />
+      <p>https://github.com/christopher-1989/meetup-rtl</p>
     </>
   )
 }
