3993964d3246bd4fa7d989b06fce05c18db2573f
Christopher McDonald <c.robin.mcdonald@gmail.com>
2024-06-19 11:31:21 +1000

    Minor styling

--- src/routes/pages/about.tsx
+++ src/routes/pages/about.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import  qrcode from '../../assets/meetup-rtl-github.svg' 
+import qrcode from '../../assets/meetup-rtl-github.svg'
 
 function About() {
   return (
     <>
       <h1>About Page</h1>
-      <img src={qrcode} alt="github-code" width={400} height={400} />
-      <p>https://github.com/christopher-1989/meetup-rtl</p>
+      <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', margin: 'auto', textWrap: 'pretty'}}>
+      <img
+        src={qrcode}
+        alt='github-code'
+        width={350}
+        height={350}
+      />
+      <a href='https://github.com/christopher-1989/meetup-rtl' style={{color: 'black'}}>
+        https://github.com/christopher-1989/meetup-rtl
+      </a>
+      </div>
     </>
   )
 }
