accd707955b1d2c1b0fd6b940d5b38feac25cb5e
Christopher McDonald <c.robin.mcdonald@gmail.com>
2024-06-19 17:06:48 +1000

    Final pass

--- src/routes/pages/about.tsx
+++ src/routes/pages/about.tsx
@@ -4,25 +4,36 @@ import porfolio from '../../assets/meetup-rtl-github.svg'
 function About() {
   return (
     <>
-      <div className='about-wrapper'>
-        <h1>Unit Testing with RTL</h1>
-        <p>Chris McDonald</p>
+      <div className='about-wrapper-top'>
+        <h1>
+          Unit Testing with RTL
+          <br/>
+          Chris McDonald  
+        </h1>
         <img
-          src={porfolio}
-          alt='My 3D Portfolio'
-          width={350}
-          height={350}
+          src={github}
+          alt='Github Repo'
+          width={250}
+          height={250}
         />
         <a
           className='svg-link'
-          href='https://christopher-1989.github.io/webgl-3d-portfolio/'
+          href='https://github.com/christopher-1989/meetup-rtl'
         >
-          https://christopher-1989.github.io/webgl-3d-portfolio/
+          https://github.com/christopher-1989/meetup-rtl
         </a>
-        <h2>Introduction</h2>
+        <div className='divider' />
+        <h2 className='intro-heading'>Introduction</h2>
         <ul>
           <li>What are unit tests?</li>
-          <li>What is React Testing Library?</li>
+          <li>
+            What is React Testing Library?
+            <br />
+            <span>
+              The more your tests resemble the way your software is used, the
+              more confidence they can give you."
+            </span>
+          </li>
           <li>
             <a
               href='https://dev.to/marktnoonan/why-i-rarely-use-getbyrole-testing-library-and-the-first-rule-of-aria-4581'
@@ -42,16 +53,16 @@ function About() {
 
       <div className='about-wrapper'>
         <img
-          src={github}
-          alt='Github Repo'
+          src={porfolio}
+          alt='My 3D Portfolio'
           width={350}
           height={350}
         />
         <a
           className='svg-link'
-          href='https://github.com/christopher-1989/meetup-rtl'
+          href='https://christopher-1989.github.io/webgl-3d-portfolio/'
         >
-          https://github.com/christopher-1989/meetup-rtl
+          https://christopher-1989.github.io/webgl-3d-portfolio/
         </a>
       </div>
     </>
